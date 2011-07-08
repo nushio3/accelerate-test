30820834ecc0a5fc5cf09eacc72785be0ea3c740
Takayuki Muranushi <muranushi@gmail.com>
2011-07-08 22:07:44 +0900

    First GPGPU example.

--- step01/Dotp.hs
+++ step01/Dotp.hs
@@ -1,5 +1,23 @@
-import Data.Array.Accelerate (Acc)
+{-# OPTIONS -Wall #-}
+import Data.Array.Accelerate (Acc, Vector, Scalar, (:.)(..))
+import Data.Array.Accelerate.CUDA (run)
 import qualified Data.Array.Accelerate as A
 
+dotp :: Vector Float -> Vector Float -> Acc (Scalar Float)
+dotp as bs = A.fold (+) 0 (A.zipWith (*) (A.use as) (A.use bs))
+
+
+shape6 :: A.DIM1
+shape6 =  A.Z :. 6
+
+xs, ys :: Vector Float
+xs = A.fromList shape6 [1,2,4,7,14,28]
+ys = A.fromList shape6 $ repeat 1
+
+ans :: Scalar Float
+ans = run $ dotp xs ys
+
+main :: IO ()
 main = do
   putStrLn "hi"
+  print ans
