2c1de00e7ac8d383800c2840b50da0bab436f950
Takayuki Muranushi <muranushi@gmail.com>
2011-07-09 17:59:43 +0900

    example of gradation generation.

--- step03/Cell.hs
+++ step03/Cell.hs
@@ -1,24 +1,33 @@
 #!/usr/bin/env runhaskell
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS -Wall #-}
-import Data.Array.Accelerate (Acc(..), Vector, Scalar, (:.)(..), Z(..))
+import Data.Array.Accelerate (Acc, Vector, Scalar, (:.)(..), Z(..), Exp)
 import Data.Array.Accelerate.CUDA (run)
 import qualified Data.Array.Accelerate.Smart as Smart
 import qualified Data.Array.Accelerate as A
 
-type Cell = A.Array A.DIM2
+-- The World is 2-dimensional! 
+type World = A.Array A.DIM2
 
-worldShape :: A.DIM2
-worldShape =  Z :. 10 :. 10 
-
-initialWorld :: Acc(Cell Int)
-initialWorld = A.generate (Smart.Const worldShape) ((\(i,j)-> 10*i + j) . unindex2 )
-
--- Turn an 'Int' expression into a rank-2 indexing expression.                       
-
+-- Turn a rank-2 indexing expression to a pair of Index.
 unindex2 :: A.Exp (Z :. Int :. Int) -> (A.Exp Int,A.Exp Int)
 unindex2 ix = let Z :. i :. j = A.unlift ix in (i,j)
 
+
+worldSize :: Num a => a
+worldSize = 10
+
+worldShape :: A.DIM2
+worldShape =  Z :. worldSize :. worldSize
+
+initialWorld :: Acc (World Float)
+initialWorld = A.generate (Smart.Const worldShape) (gradation . unindex2 )
+
+gradation :: (A.Exp Int,A.Exp Int) -> Exp Float
+gradation (i,j) = fi / worldSize + fj / worldSize / worldSize
+  where
+    fi = A.fromIntegral i
+    fj = A.fromIntegral j
 
 main :: IO ()
 main = do
