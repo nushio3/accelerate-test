b4fc7bf4626873c1b0bc646d5bb14988bcd3b985
Takayuki Muranushi <muranushi@gmail.com>
2011-07-12 13:23:44 +0900

    readable input version.

--- step01/Dotp.hs
+++ step01/Dotp.hs
@@ -4,19 +4,30 @@ import Data.Array.Accelerate (Acc, Vector, Scalar, (:.)(..))
 import Data.Array.Accelerate.CUDA (run)
 import qualified Data.Array.Accelerate as A
 
-dotp :: Vector Float -> Vector Float -> Acc (Scalar Float)
-dotp as bs = A.fold (+) 0 (A.zipWith (*) (A.use as) (A.use bs))
-
-shape6 :: A.DIM1
-shape6 =  A.Z :. 6
+myShape :: A.DIM1
+myShape =  A.Z :. 10
 
 xs, ys :: Vector Float
-xs = A.fromList shape6 [1,2,4,7,14,28]
-ys = A.fromList shape6 $ repeat 1
+xs = A.fromList myShape [83,72,73,78,82,89,65,75,85,33]
+ys = A.fromList myShape [73,75,65,32,77,85,83,85,77,69]
+
+accXs, accYs :: Acc (Vector Float)
+accXs = A.use xs
+accYs = A.use ys
+
+dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
+dotp as bs = A.fold (+) 0 (A.zipWith (*) as bs)
+
+accAns :: Acc (Scalar Float)
+accAns = dotp accXs accYs
 
 ans :: Scalar Float
-ans = run $ dotp xs ys
+ans = run $ accAns
 
 main :: IO ()
 main = do
-  print ans
+  putStrLn $ "xs    : " ++ show xs
+  putStrLn $ "accXs : " ++ show accXs
+  putStrLn $ "accAns: " ++ show accAns
+  putStrLn $ "ans   : " ++ show ans
+
