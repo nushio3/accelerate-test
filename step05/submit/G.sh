b3c6346eb5e4ab3e67ac6b480f87cc67bfdd7129
Takayuki Muranushi <muranushi@gmail.com>
2011-07-18 16:37:37 +0900

    submit script moved under a folder.

--- step05/submit/G.sh
+++ step05/submit/G.sh
@@ -1,4 +1,5 @@
 #t2sub -q G -W group_list=t2g-ppc-all -l select=1:gpus=3:mem=21gb -l walltime=24:00:00 ./tools/batch_execute.sh
 #t2sub -q G -W group_list=t2g-ppc-all -l select=1:gpus=1:mem=21gb -l walltime=24:00:00 ./tools/batch_execute.sh
-t2sub -N autotuning -q G -W group_list=t2g-ppc-all -l select=1:gpus=1:mem=21gb -l walltime=24:00:00 ./submit/exec_G.sh 
+t2sub -N autotuning -q G -W group_list=t2g-ppc-all -l select=1:gpus=1:mem=21gb -l walltime=24:00:00 $@
+
 
--- step05/submit_G.sh
+++
@@ -1,4 +0,0 @@
-#t2sub -q G -W group_list=t2g-ppc-all -l select=1:gpus=3:mem=21gb -l walltime=24:00:00 ./tools/batch_execute.sh
-#t2sub -q G -W group_list=t2g-ppc-all -l select=1:gpus=1:mem=21gb -l walltime=24:00:00 ./tools/batch_execute.sh
-t2sub -N autotuning -q G -W group_list=t2g-ppc-all -l select=1:gpus=1:mem=21gb -l walltime=24:00:00 ./batch_execute.sh 
-
